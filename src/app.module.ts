147f5a0188cac65a2720fd9097375c6bd039e87d
Abhijit47 <akarmakar846@gmail.com>
2026-09-15 00:05:31 +0530

    refactor(app): integrate common middleware globally
    Implement NestModule in AppModule to apply CommonMiddleware to all
    routes. This establishes a centralized middleware layer for
    application-wide processing.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
+import { CommonMiddleware } from './common/common.middleware';
 
 @Module({
   imports: [BlogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CommonMiddleware).forRoutes('*');
+  }
+}
